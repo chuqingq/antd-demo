6af169960a8f54c7002b65ace3e1b5479bdd93fe
chuqq <chuqingq@qq.com>
2019-06-03 21:22:41 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
   <div id="app">
     <div>
-      <a-row>
-        <a-date-picker id="a" @change="onChange" placeholder="Select date"/>
-      </a-row>
-      <a-row>
-        <a-month-picker id="d" @change="onChange" placeholder="Select month"/>
-      </a-row>
-      <a-row>
-        <a-range-picker id="b" @change="onChange"/>
-      </a-row>
-      <a-row>
-        <a-week-picker @change="onChange" placeholder="Select week"/>
-      </a-row>
-    </div>
-
-    <div>
-      <a-button type="primary">Primary</a-button>
-      <a-button>Default</a-button>
-      <a-button type="dashed">Dashed</a-button>
-      <a-button type="danger">Danger</a-button>
+      <a-layout>
+        <a-layout-header>Header</a-layout-header>
+        <a-layout>
+          <a-layout-sider>Sider</a-layout-sider>
+          <a-layout-content>
+            <a-row>
+              <a-date-picker id="a" @change="onChange" placeholder="Select date"/>
+            </a-row>
+            <a-row>
+              <a-month-picker id="d" @change="onChange" placeholder="Select month"/>
+            </a-row>
+            <a-row>
+              <a-range-picker id="b" @change="onChange"/>
+            </a-row>
+            <a-row>
+              <a-week-picker @change="onChange" placeholder="Select week"/>
+            </a-row>
+            <a-button type="primary">Primary</a-button>
+            <a-button>Default</a-button>
+            <a-button type="dashed">Dashed</a-button>
+            <a-button type="danger">Danger</a-button>
+          </a-layout-content>
+        </a-layout>
+        <a-layout-footer>Footer</a-layout-footer>
+      </a-layout>
     </div>
   </div>
 </template>
